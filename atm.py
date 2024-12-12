53297cd27eae6ef5604cdcd0e5d58ed41a1e96e9
Utkarshadley <145756963+utkarshadley@users.noreply.github.com>
2024-12-12 21:22:56 +0530

    Add files via upload

---
+++ atm.py
@@ -0,0 +1,56 @@
+
+# import speech_recognition as sr 
+# import pyttsx3
+# engine = pyttsx3.init() 
+# def speak(text):
+#     engine.say(text)
+#     engine.runAndWait()  
+
+# if __name__ == "__main__":  
+#     speak("Welcome to punjab national bank atm service")
+#     speak("Enter your  4- digit pin")
+# pin=4444
+# a= int(input("Enter the pin number: "))
+# if a==pin:
+#     speak("Choose your choice")
+#     print("Welcome to Punjab National Bank ATM service!")
+#     print("1./ Checking Balance")
+#     print("2./ Withdrawal Money")
+#     print("3./ Deposit Money")
+#     print("6./ Exit")
+#     choice=int(input ("Enter Your choice: "))
+#     if choice==1:
+#         print("Checking Bbalance")
+#         speak("Checking Bbalance")
+#         speak("Your balance is 10000 rs")
+#         print("Your balance is 10000 rs")
+        
+#     elif choice==2:
+#         print("Withdrawal Money")
+#         speak("Withdrawl Money")
+#         amount=int(input("Enter withdrawing amount:"))
+#         if amount<0:
+#             print("Invalid amount")
+#         elif amount<=10000 :
+#             print("Your available balance is",10000-amount)
+        
+                
+            
+#         else:
+#             print("Invalid amount")
+#             speak("Invalid amount")
+#     elif choice==3:
+#         print("Deposit Money")
+#         speak("Deposit Money")
+#         amount=int(input("Enter the depositing money:"))
+#         if amount<=0:
+#             print("Invalid amount")
+#         else:
+#          print("Your new balance is",10000+amount)
+            
+#     else:
+#             print("Invalid amount")
+#             speak("Invalid amount")
+
+
+
